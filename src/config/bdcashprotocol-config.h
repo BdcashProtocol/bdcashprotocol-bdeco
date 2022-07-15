5fcb6af714fb1566f0fb6466152a3fbe2d8a462d
Fabianno Limma <fabiano.alexandrebiano@gmail.com>
2022-07-15 08:27:24 -0300

    changes #2
    - new version 1.1.0
    - protocol changed to 70923
    - removed string(build)

--- src/config/bdcashprotocol-config.h
+++ src/config/bdcashprotocol-config.h
@@ -18,7 +18,7 @@
 #define CLIENT_VERSION_MAJOR 1
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 0
+#define CLIENT_VERSION_MINOR 1
 
 /* Build revision */
 #define CLIENT_VERSION_REVISION 0
@@ -290,7 +290,7 @@
 #define PACKAGE_NAME "BDCashProtocol"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "BDCashProtocol 1.0.0"
+#define PACKAGE_STRING "BDCashProtocol 1.1.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bdcashprotocol"
@@ -299,7 +299,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.0"
+#define PACKAGE_VERSION "1.1.0"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
